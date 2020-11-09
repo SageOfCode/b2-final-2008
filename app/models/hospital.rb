f94f8a0e40d4145d0c2e12db194382c709ab01f4
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-09 10:09:38 -0700

    Add hospital model methods and test

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,12 @@
 class Hospital < ApplicationRecord
   validates_presence_of :name
   has_many :doctors
+
+  def doctor_count
+    doctors.count
+  end
+
+  def doctor_universities
+    doctors.pluck(:university).uniq.join(", ")
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,53 @@ RSpec.describe Hospital, type: :model do
   describe "relationships" do
     it {should have_many :doctors}
   end 
+
+  describe "instance method tests" do 
+    it 'knows doctor count' do
+      hospital = Hospital.create!(
+        name: "Weenie Hut General"
+      )
+      doctor1 = hospital.doctors.create!(
+        name: "Dr. Manowar",
+        specialty: "Jellyfish stings",
+        university: "Rock Bottom University"
+      )
+      doctor2 = hospital.doctors.create!(
+        name: "Dr. Manowar",
+        specialty: "Cooking related injuries",
+        university: "Bikini Bottom University"
+      )
+      doctor3 = hospital.doctors.create!(
+        name: "Patrick Star",
+        specialty: "Injuries from sitting too long",
+        university: "Bikini Bottom University"
+      )
+
+      expect(hospital.doctor_count).to eq(3)
+    end
+
+    it 'knows doctor universities' do
+      hospital = Hospital.create!(
+        name: "Weenie Hut General"
+      )
+      doctor1 = hospital.doctors.create!(
+        name: "Dr. Manowar",
+        specialty: "Jellyfish stings",
+        university: "Rock Bottom University"
+      )
+      doctor2 = hospital.doctors.create!(
+        name: "Dr. Manowar",
+        specialty: "Cooking related injuries",
+        university: "Bikini Bottom University"
+      )
+      doctor3 = hospital.doctors.create!(
+        name: "Patrick Star",
+        specialty: "Injuries from sitting too long",
+        university: "Bikini Bottom University"
+      )
+
+      expect(hospital.doctor_universities).to eq("Rock Bottom University, Bikini Bottom University")
+    end
+
+  end
 end
