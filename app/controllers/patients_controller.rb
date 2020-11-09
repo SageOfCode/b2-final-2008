a4d2209f2cff1a4d3416c5cfa6862077f491a675
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-09 11:32:30 -0700

    Added index to patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,12 @@
 class PatientsController < ApplicationController
- def destroy
+  def destroy
     patient = Patient.find(params[:patient_id])
     patient.destroy
     doctor = Doctor.find(params[:doctor_id])
     redirect_to "/doctors/#{doctor.id}"
+  end
+
+  def index 
+    @patients = Patient.all
   end
 end 
