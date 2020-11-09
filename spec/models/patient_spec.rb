46ef04e37360dd251d03b095f57e44fc6648b258
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-09 11:36:20 -0700

    Add model test for patients by age extension

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe Patient, type: :model do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end 
+  describe "class methods" do 
+    it "knows patients by age" do 
+      hospital = Hospital.create!(
+        name: "Weenie Hut General"
+      )
+      doctor = hospital.doctors.create!(
+        name: "Dr. Manowar",
+        specialty: "Jellyfish stings",
+        university: "Rock Bottom University"
+      )
+      patient1 = doctor.patients.create!(
+        name: "Spongebob Squarepants",
+        age: "22"
+      )
+      patient2 = doctor.patients.create!(
+        name: "Patrick Star",
+        age: "24"
+      )
+      patient3 = doctor.patients.create!(
+        name: "Squidward Tenticles",
+        age: "26"
+      )
+
+      expect(Patient.patients_by_age).to eq([patient3, patient2, patient1])
+    end
+  end
 end
